3bb5e93451e85e88ad7708c19767f54b51c5e951
Kristian Coulson <kristiancoulson@gmail.com>
2024-02-28 19:46:48 +0000

    added react

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { html } from "hono/html";
+
+const app = new Hono();
+
+app.get("*", (c) => {
+  return c.html(html` <html>
+    <head>
+      <link href="/static/style.css" rel="stylesheet" />
+      <script type="module" src="/src/client/index.tsx"></script>
+    </head>
+    <body>
+      <div id="root"></div>
+    </body>
+  </html>`);
+});
+
+export default app;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import build from '@hono/vite-cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    viteReact(),
     build(),
     devServer({
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+      entry: "src/index.ts",
+    }),
+  ],
+});
