6fbaf9e4cc791a68c263d8777d11ff45f57e1d91
Kristian Coulson <kristiancoulson@gmail.com>
2024-02-28 19:55:08 +0000

    test static js

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,12 @@ app.get("/", (c) => {
   return c.html(html` <html>
     <head>
       <link href="/static/style.css" rel="stylesheet" />
-      <script type="module" src="/src/client/index.tsx"></script>
+      <script
+        type="module"
+        src="${import.meta.env.PROD
+          ? "/static/client.js"
+          : "/src/client/index.tsx"}"
+      ></script>
     </head>
     <body>
       <div id="root"></div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,26 @@ import devServer from "@hono/vite-dev-server";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    viteReact(),
-    build(),
-    devServer({
-      entry: "src/index.tsx",
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      build: {
+        rollupOptions: {
+          input: "./src/client/index.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      plugins: [
+        build(),
+        devServer({
+          entry: "src/index.tsx",
+        }),
+      ],
+    };
+  }
 });
